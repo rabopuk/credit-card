e9ed3037d8cde280ee433e8747a179a8b29ee06a
rabopuk <rabopuk@gmail.com>
2024-04-18 17:41:32 +0400

    fix: still not working

--- dist/index.63aff760.js
+++ dist/index.63aff760.js
@@ -616,187 +616,100 @@ parcelHelpers.export(exports, "createElements", ()=>createElements);
 var _redom = require("redom");
 const createElements = ()=>{
     const paymentTitle = (0, _redom.el)("div.payment-title", (0, _redom.el)("h1", "Payment Information"));
-    const ccsingle = (0, _redom.el)("div#ccsingle", (0, _redom.el)("img", {
-        src: "../img/reshot-icon-telegram-LSZVDUKX6M.svg",
-        alt: "\u043B\u043E\u0433\u043E\u0442\u0438\u043F"
-    })); // logo
+    const ccsingle = (0, _redom.el)("div#ccsingle");
+    ccsingle.innerHTML = `
+    <img src="../img/reshot-icon-telegram-LSZVDUKX6M.svg" alt="\u{43B}\u{43E}\u{433}\u{43E}\u{442}\u{438}\u{43F}">
+  `; // logo
     const cardFront = (0, _redom.el)("svg#cardfront", {
-        version: "1.1",
-        xmlns: "http://www.w3.org/2000/svg",
-        xmlnsXlink: "http://www.w3.org/1999/xlink",
-        width: "750",
-        height: "471",
-        viewBox: "0 0 750 471",
-        style: "enable-background:new 0 0 750 471;",
-        xmlSpace: "preserve"
-    }, [
-        (0, _redom.el)("g", {
-            id: "Front"
-        }, [
-            (0, _redom.el)("g", {
-                id: "CardBackground"
-            }, [
-                (0, _redom.el)("g", {
-                    id: "Page-1_1_"
-                }, [
-                    (0, _redom.el)("g", {
-                        id: "amex_1_"
-                    }, [
-                        (0, _redom.el)("path", {
-                            id: "Rectangle-1_1_",
-                            class: "lightcolor grey",
-                            d: "M40,0h670c22.1,0,40,17.9,40,40v391c0,22.1-17.9,40-40,40H40c-22.1,0-40-17.9-40-40V40 C0,17.9,17.9,0,40,0z"
-                        })
-                    ])
-                ]),
-                (0, _redom.el)("path", {
-                    class: "darkcolor greydark",
-                    d: "M750,431V193.2c-217.6-57.5-556.4-13.5-750,24.9V431c0,22.1,17.9,40,40,40h670C732.1,471,750,453.1,750,431z"
-                })
-            ]),
-            (0, _redom.el)("text", {
-                transform: "matrix(1 0 0 1 60.106 295.0121)",
-                id: "svgnumber",
-                class: "st2 st3 st4"
-            }, "0123 4567 8910 1112"),
-            (0, _redom.el)("text", {
-                transform: "matrix(1 0 0 1 54.1064 428.1723)",
-                id: "svgname",
-                class: "st2 st5 st6"
-            }, "\u0418\u0412\u0410\u041D \u0418\u0412\u0410\u041D\u041E\u0412"),
-            (0, _redom.el)("text", {
-                transform: "matrix(1 0 0 1 54.1074 389.8793)",
-                class: "st7 st5 st8"
-            }, "cardholder name"),
-            (0, _redom.el)("text", {
-                transform: "matrix(1 0 0 1 479.7754 388.8793)",
-                class: "st7 st5 st8"
-            }, "expiration"),
-            (0, _redom.el)("text", {
-                transform: "matrix(1 0 0 1 65.1054 241.5)",
-                class: "st7 st5 st8"
-            }, "card number"),
-            (0, _redom.el)("g", [
-                (0, _redom.el)("text", {
-                    transform: "matrix(1 0 0 1 574.4219 433.8095)",
-                    id: "svgexpire",
-                    class: "st2 st5 st9"
-                }, "01/23"),
-                (0, _redom.el)("text", {
-                    transform: "matrix(1 0 0 1 479.3848 417.0097)",
-                    class: "st2 st10 st11"
-                }, "VALID"),
-                (0, _redom.el)("text", {
-                    transform: "matrix(1 0 0 1 479.3848 435.6762)",
-                    class: "st2 st10 st11"
-                }, "THRU"),
-                (0, _redom.el)("polygon", {
-                    class: "st2",
-                    points: "554.5,421 540.4,414.2 540.4,427.9"
-                })
-            ]),
-            (0, _redom.el)("g", {
-                id: "cchip"
-            }, [
-                (0, _redom.el)("g", [
-                    (0, _redom.el)("path", {
-                        class: "st2",
-                        d: "M168.1,143.6H82.9c-10.2,0-18.5-8.3-18.5-18.5V74.9c0-10.2,8.3-18.5,18.5-18.5h85.3 c10.2,0,18.5,8.3,18.5,18.5v50.2C186.6,135.3,178.3,143.6,168.1,143.6z"
-                    })
-                ])
-            ])
-        ])
-    ]);
-    const cardBack = (0, _redom.el)("svg#cardback", {
-        version: "1.1",
-        xmlns: "http://www.w3.org/2000/svg",
-        xmlnsXlink: "http://www.w3.org/1999/xlink",
-        width: "750",
-        height: "471",
-        viewBox: "0 0 750 471",
-        style: "enable-background:new 0 0 750 471;",
-        xmlSpace: "preserve"
-    }, [
-        (0, _redom.el)("g", {
-            id: "Back"
-        }, [
-            (0, _redom.el)("g", {
-                id: "Page-1_2_"
-            }, [
-                (0, _redom.el)("g", {
-                    id: "amex_2_"
-                }, [
-                    (0, _redom.el)("path", {
-                        id: "Rectangle-1_2_",
-                        class: "darkcolor greydark",
-                        d: "M40,0h670c22.1,0,40,17.9,40,40v391c0,22.1-17.9,40-40,40H40c-22.1,0-40-17.9-40-40V40 C0,17.9,17.9,0,40,0z"
-                    })
-                ])
-            ]),
-            (0, _redom.el)("rect", {
-                y: "61.6",
-                class: "st2",
-                width: "750",
-                height: "78"
-            }),
-            (0, _redom.el)("g", [
-                (0, _redom.el)("path", {
-                    class: "st3",
-                    d: "M701.1,249.1H48.9c-3.3,0-6-2.7-6-6v-52.5c0-3.3,2.7-6,6-6h652.1c3.3,0,6,2.7,6,6v52.5 C707.1,246.4,704.4,249.1,701.1,249.1z"
-                }),
-                (0, _redom.el)("rect", {
-                    x: "42.9",
-                    y: "198.6",
-                    class: "st4",
-                    width: "664.1",
-                    height: "10.5"
-                }),
-                (0, _redom.el)("rect", {
-                    x: "42.9",
-                    y: "224.5",
-                    class: "st4",
-                    width: "664.1",
-                    height: "10.5"
-                }),
-                (0, _redom.el)("path", {
-                    class: "st5",
-                    d: "M701.1,184.6H618h-8h-10v64.5h10h8h83.1c3.3,0,6-2.7,6-6v-52.5 C707.1,187.3,704.4,184.6,701.1,184.6z"
-                })
-            ]),
-            (0, _redom.el)("text", {
-                transform: "matrix(1 0 0 1 621.999 227.2734)",
-                id: "svgsecurity",
-                class: "st6 st7"
-            }, "985"),
-            (0, _redom.el)("g", {
-                class: "st8"
-            }, [
-                (0, _redom.el)("text", {
-                    transform: "matrix(1 0 0 1 518.083 280.0879)",
-                    class: "st9 st6 st10"
-                }, "security code")
-            ]),
-            (0, _redom.el)("rect", {
-                x: "58.1",
-                y: "378.6",
-                class: "st11",
-                width: "375.5",
-                height: "13.5"
-            }),
-            (0, _redom.el)("rect", {
-                x: "58.1",
-                y: "405.6",
-                class: "st11",
-                width: "421.7",
-                height: "13.5"
-            }),
-            (0, _redom.el)("text", {
-                transform: "matrix(1 0 0 1 59.5073 228.6099)",
-                id: "svgnameback",
-                class: "st12 st13"
-            }, "\u0418\u0432\u0430\u043D \u0418\u0432\u0430\u043D\u043E\u0432")
-        ])
-    ]);
+        "version": "1.1",
+        "id": "cardfront",
+        "xmlns": "http://www.w3.org/2000/svg",
+        "xmlns:xlink": "http://www.w3.org/1999/xlink",
+        "x": "0px",
+        "y": "0px",
+        "viewBox": "0 0 750 471",
+        "style": "enable-background:new 0 0 750 471;",
+        "xml:space": "preserve"
+    });
+    const frontSvgHTML = `
+    <g id="Front">
+      <g id="CardBackground">
+        <g id="Page-1_1_">
+          <g id="amex_1_">
+            <path id="Rectangle-1_1_" class="lightcolor grey" d="M40,0h670c22.1,0,40,17.9,40,40v391c0,22.1-17.9,40-40,40H40c-22.1,0-40-17.9-40-40V40
+              C0,17.9,17.9,0,40,0z" />
+          </g>
+        </g>
+        <path class="darkcolor greydark" d="M750,431V193.2c-217.6-57.5-556.4-13.5-750,24.9V431c0,22.1,17.9,40,40,40h670C732.1,471,750,453.1,750,431z" />
+      </g>
+      <text transform="matrix(1 0 0 1 60.106 295.0121)" id="svgnumber" class="st2 st3 st4">0123 4567 8910 1112</text>
+      <text transform="matrix(1 0 0 1 54.1064 428.1723)" id="svgname" class="st2 st5 st6">\u{418}\u{412}\u{410}\u{41D} \u{418}\u{412}\u{410}\u{41D}\u{41E}\u{412}</text>
+      <text transform="matrix(1 0 0 1 54.1074 389.8793)" class="st7 st5 st8">cardholder name</text>
+      <text transform="matrix(1 0 0 1 479.7754 388.8793)" class="st7 st5 st8">expiration</text>
+      <text transform="matrix(1 0 0 1 65.1054 241.5)" class="st7 st5 st8">card number</text>
+      <g>
+        <text transform="matrix(1 0 0 1 574.4219 433.8095)" id="svgexpire" class="st2 st5 st9">01/23</text>
+        <text transform="matrix(1 0 0 1 479.3848 417.0097)" class="st2 st10 st11">VALID</text>
+        <text transform="matrix(1 0 0 1 479.3848 435.6762)" class="st2 st10 st11">THRU</text>
+        <polygon class="st2" points="554.5,421 540.4,414.2 540.4,427.9" />
+      </g>
+      <g id="cchip">
+        <g>
+          <path class="st2" d="M168.1,143.6H82.9c-10.2,0-18.5-8.3-18.5-18.5V74.9c0-10.2,8.3-18.5,18.5-18.5h85.3
+              c10.2,0,18.5,8.3,18.5,18.5v50.2C186.6,135.3,178.3,143.6,168.1,143.6z" />
+        </g>
+      </g>
+    </g>
+    <g id="Back">
+    </g>
+`;
+    cardFront.innerHTML = frontSvgHTML;
+    const cardBack = (0, _redom.el)("svg#cardfront", {
+        "version": "1.1",
+        "id": "cardback",
+        "xmlns": "http://www.w3.org/2000/svg",
+        "xmlns:xlink": "http://www.w3.org/1999/xlink",
+        "x": "0px",
+        "y": "0px",
+        "viewBox": "0 0 750 471",
+        "style": "enable-background:new 0 0 750 471;",
+        "xml:space": "preserve"
+    });
+    const backSvgHTML = `
+    <g id="Front">
+      <line class="st0" x1="35.3" y1="10.4" x2="36.7" y2="11" />
+    </g>
+    <g id="Back">
+      <g id="Page-1_2_">
+        <g id="amex_2_">
+          <path id="Rectangle-1_2_" class="darkcolor greydark" d="M40,0h670c22.1,0,40,17.9,40,40v391c0,22.1-17.9,40-40,40H40c-22.1,0-40-17.9-40-40V40
+        C0,17.9,17.9,0,40,0z" />
+        </g>
+      </g>
+      <rect y="61.6" class="st2" width="750" height="78" />
+      <g>
+        <path class="st3" d="M701.1,249.1H48.9c-3.3,0-6-2.7-6-6v-52.5c0-3.3,2.7-6,6-6h652.1c3.3,0,6,2.7,6,6v52.5
+                  C707.1,246.4,704.4,249.1,701.1,249.1z" />
+        <rect x="42.9" y="198.6" class="st4" width="664.1"
+          height="10.5" />
+        <rect x="42.9" y="224.5" class="st4" width="664.1"
+          height="10.5" />
+        <path class="st5"
+          d="M701.1,184.6H618h-8h-10v64.5h10h8h83.1c3.3,0,6-2.7,6-6v-52.5C707.1,187.3,704.4,184.6,701.1,184.6z" />
+      </g>
+      <text transform="matrix(1 0 0 1 621.999 227.2734)" id="svgsecurity"
+        class="st6 st7">985</text>
+      <g class="st8">
+        <text transform="matrix(1 0 0 1 518.083 280.0879)"
+          class="st9 st6 st10">security code</text>
+      </g>
+      <rect x="58.1" y="378.6" class="st11" width="375.5" height="13.5" />
+      <rect x="58.1" y="405.6" class="st11" width="421.7" height="13.5" />
+      <text transform="matrix(1 0 0 1 59.5073 228.6099)" id="svgnameback"
+        class="st12 st13">\u{418}\u{432}\u{430}\u{43D} \u{418}\u{432}\u{430}\u{43D}\u{43E}\u{432}</text>
+    </g>
+  `;
+    cardBack.innerHTML = backSvgHTML;
     const formContainer = (0, _redom.el)("div.form-container", [
         (0, _redom.el)("div.field-container", [
             (0, _redom.el)("label", {
--- modules/createElements.js
+++ modules/createElements.js
@@ -3,78 +3,106 @@ import { el, setChildren } from 'redom';
 
 export const createElements = () => {
   const paymentTitle = el('div.payment-title', el('h1', 'Payment Information'));
-  const ccsingle = el('div#ccsingle', el('img', { src: '../img/reshot-icon-telegram-LSZVDUKX6M.svg', alt: 'логотип' })); // logo
+  const ccsingle = el('div#ccsingle');
+  ccsingle.innerHTML = `
+    <img src="../img/reshot-icon-telegram-LSZVDUKX6M.svg" alt="логотип">
+  `; // logo
 
   const cardFront = el('svg#cardfront', {
-    version: '1.1',
-    xmlns: 'http://www.w3.org/2000/svg',
-    xmlnsXlink: 'http://www.w3.org/1999/xlink',
-    width: '750',
-    height: '471',
-    viewBox: '0 0 750 471',
-    style: 'enable-background:new 0 0 750 471;',
-    xmlSpace: 'preserve',
-  }, [
-    el('g', { id: 'Front' }, [
-      el('g', { id: 'CardBackground' }, [
-        el('g', { id: 'Page-1_1_' }, [
-          el('g', { id: 'amex_1_' }, [
-            el('path', { id: 'Rectangle-1_1_', class: 'lightcolor grey', d: 'M40,0h670c22.1,0,40,17.9,40,40v391c0,22.1-17.9,40-40,40H40c-22.1,0-40-17.9-40-40V40 C0,17.9,17.9,0,40,0z' }),
-          ]),
-        ]),
-        el('path', { class: 'darkcolor greydark', d: 'M750,431V193.2c-217.6-57.5-556.4-13.5-750,24.9V431c0,22.1,17.9,40,40,40h670C732.1,471,750,453.1,750,431z' }),
-      ]),
-      el('text', { transform: 'matrix(1 0 0 1 60.106 295.0121)', id: 'svgnumber', class: 'st2 st3 st4' }, '0123 4567 8910 1112'),
-      el('text', { transform: 'matrix(1 0 0 1 54.1064 428.1723)', id: 'svgname', class: 'st2 st5 st6' }, 'ИВАН ИВАНОВ'),
-      el('text', { transform: 'matrix(1 0 0 1 54.1074 389.8793)', class: 'st7 st5 st8' }, 'cardholder name'),
-      el('text', { transform: 'matrix(1 0 0 1 479.7754 388.8793)', class: 'st7 st5 st8' }, 'expiration'),
-      el('text', { transform: 'matrix(1 0 0 1 65.1054 241.5)', class: 'st7 st5 st8' }, 'card number'),
-      el('g', [
-        el('text', { transform: 'matrix(1 0 0 1 574.4219 433.8095)', id: 'svgexpire', class: 'st2 st5 st9' }, '01/23'),
-        el('text', { transform: 'matrix(1 0 0 1 479.3848 417.0097)', class: 'st2 st10 st11' }, 'VALID'),
-        el('text', { transform: 'matrix(1 0 0 1 479.3848 435.6762)', class: 'st2 st10 st11' }, 'THRU'),
-        el('polygon', { class: 'st2', points: '554.5,421 540.4,414.2 540.4,427.9' }),
-      ]),
-      el('g', { id: 'cchip' }, [
-        el('g', [
-          el('path', { class: 'st2', d: 'M168.1,143.6H82.9c-10.2,0-18.5-8.3-18.5-18.5V74.9c0-10.2,8.3-18.5,18.5-18.5h85.3 c10.2,0,18.5,8.3,18.5,18.5v50.2C186.6,135.3,178.3,143.6,168.1,143.6z' }),
-        ]),
-      ]),
-    ]),
-  ]);
+    'version': '1.1',
+    'id': 'cardfront',
+    'xmlns': 'http://www.w3.org/2000/svg',
+    'xmlns:xlink': 'http://www.w3.org/1999/xlink',
+    'x': '0px',
+    'y': '0px',
+    'viewBox': '0 0 750 471',
+    'style': 'enable-background:new 0 0 750 471;',
+    'xml:space': 'preserve',
+  });
 
-  const cardBack = el('svg#cardback', {
-    version: '1.1',
-    xmlns: 'http://www.w3.org/2000/svg',
-    xmlnsXlink: 'http://www.w3.org/1999/xlink',
-    width: '750',
-    height: '471',
-    viewBox: '0 0 750 471',
-    style: 'enable-background:new 0 0 750 471;',
-    xmlSpace: 'preserve',
-  }, [
-    el('g', { id: 'Back' }, [
-      el('g', { id: 'Page-1_2_' }, [
-        el('g', { id: 'amex_2_' }, [
-          el('path', { id: 'Rectangle-1_2_', class: 'darkcolor greydark', d: 'M40,0h670c22.1,0,40,17.9,40,40v391c0,22.1-17.9,40-40,40H40c-22.1,0-40-17.9-40-40V40 C0,17.9,17.9,0,40,0z' }),
-        ]),
-      ]),
-      el('rect', { y: '61.6', class: 'st2', width: '750', height: '78' }),
-      el('g', [
-        el('path', { class: 'st3', d: 'M701.1,249.1H48.9c-3.3,0-6-2.7-6-6v-52.5c0-3.3,2.7-6,6-6h652.1c3.3,0,6,2.7,6,6v52.5 C707.1,246.4,704.4,249.1,701.1,249.1z' }),
-        el('rect', { x: '42.9', y: '198.6', class: 'st4', width: '664.1', height: '10.5' }),
-        el('rect', { x: '42.9', y: '224.5', class: 'st4', width: '664.1', height: '10.5' }),
-        el('path', { class: 'st5', d: 'M701.1,184.6H618h-8h-10v64.5h10h8h83.1c3.3,0,6-2.7,6-6v-52.5 C707.1,187.3,704.4,184.6,701.1,184.6z' }),
-      ]),
-      el('text', { transform: 'matrix(1 0 0 1 621.999 227.2734)', id: 'svgsecurity', class: 'st6 st7' }, '985'),
-      el('g', { class: 'st8' }, [
-        el('text', { transform: 'matrix(1 0 0 1 518.083 280.0879)', class: 'st9 st6 st10' }, 'security code'),
-      ]),
-      el('rect', { x: '58.1', y: '378.6', class: 'st11', width: '375.5', height: '13.5' }),
-      el('rect', { x: '58.1', y: '405.6', class: 'st11', width: '421.7', height: '13.5' }),
-      el('text', { transform: 'matrix(1 0 0 1 59.5073 228.6099)', id: 'svgnameback', class: 'st12 st13' }, 'Иван Иванов'),
-    ]),
-  ]);
+  const frontSvgHTML = `
+    <g id="Front">
+      <g id="CardBackground">
+        <g id="Page-1_1_">
+          <g id="amex_1_">
+            <path id="Rectangle-1_1_" class="lightcolor grey" d="M40,0h670c22.1,0,40,17.9,40,40v391c0,22.1-17.9,40-40,40H40c-22.1,0-40-17.9-40-40V40
+              C0,17.9,17.9,0,40,0z" />
+          </g>
+        </g>
+        <path class="darkcolor greydark" d="M750,431V193.2c-217.6-57.5-556.4-13.5-750,24.9V431c0,22.1,17.9,40,40,40h670C732.1,471,750,453.1,750,431z" />
+      </g>
+      <text transform="matrix(1 0 0 1 60.106 295.0121)" id="svgnumber" class="st2 st3 st4">0123 4567 8910 1112</text>
+      <text transform="matrix(1 0 0 1 54.1064 428.1723)" id="svgname" class="st2 st5 st6">ИВАН ИВАНОВ</text>
+      <text transform="matrix(1 0 0 1 54.1074 389.8793)" class="st7 st5 st8">cardholder name</text>
+      <text transform="matrix(1 0 0 1 479.7754 388.8793)" class="st7 st5 st8">expiration</text>
+      <text transform="matrix(1 0 0 1 65.1054 241.5)" class="st7 st5 st8">card number</text>
+      <g>
+        <text transform="matrix(1 0 0 1 574.4219 433.8095)" id="svgexpire" class="st2 st5 st9">01/23</text>
+        <text transform="matrix(1 0 0 1 479.3848 417.0097)" class="st2 st10 st11">VALID</text>
+        <text transform="matrix(1 0 0 1 479.3848 435.6762)" class="st2 st10 st11">THRU</text>
+        <polygon class="st2" points="554.5,421 540.4,414.2 540.4,427.9" />
+      </g>
+      <g id="cchip">
+        <g>
+          <path class="st2" d="M168.1,143.6H82.9c-10.2,0-18.5-8.3-18.5-18.5V74.9c0-10.2,8.3-18.5,18.5-18.5h85.3
+              c10.2,0,18.5,8.3,18.5,18.5v50.2C186.6,135.3,178.3,143.6,168.1,143.6z" />
+        </g>
+      </g>
+    </g>
+    <g id="Back">
+    </g>
+`;
+
+  cardFront.innerHTML = frontSvgHTML;
+
+  const cardBack = el('svg#cardfront', {
+    'version': '1.1',
+    'id': 'cardback',
+    'xmlns': 'http://www.w3.org/2000/svg',
+    'xmlns:xlink': 'http://www.w3.org/1999/xlink',
+    'x': '0px',
+    'y': '0px',
+    'viewBox': '0 0 750 471',
+    'style': 'enable-background:new 0 0 750 471;',
+    'xml:space': 'preserve',
+  });
+
+  const backSvgHTML = `
+    <g id="Front">
+      <line class="st0" x1="35.3" y1="10.4" x2="36.7" y2="11" />
+    </g>
+    <g id="Back">
+      <g id="Page-1_2_">
+        <g id="amex_2_">
+          <path id="Rectangle-1_2_" class="darkcolor greydark" d="M40,0h670c22.1,0,40,17.9,40,40v391c0,22.1-17.9,40-40,40H40c-22.1,0-40-17.9-40-40V40
+        C0,17.9,17.9,0,40,0z" />
+        </g>
+      </g>
+      <rect y="61.6" class="st2" width="750" height="78" />
+      <g>
+        <path class="st3" d="M701.1,249.1H48.9c-3.3,0-6-2.7-6-6v-52.5c0-3.3,2.7-6,6-6h652.1c3.3,0,6,2.7,6,6v52.5
+                  C707.1,246.4,704.4,249.1,701.1,249.1z" />
+        <rect x="42.9" y="198.6" class="st4" width="664.1"
+          height="10.5" />
+        <rect x="42.9" y="224.5" class="st4" width="664.1"
+          height="10.5" />
+        <path class="st5"
+          d="M701.1,184.6H618h-8h-10v64.5h10h8h83.1c3.3,0,6-2.7,6-6v-52.5C707.1,187.3,704.4,184.6,701.1,184.6z" />
+      </g>
+      <text transform="matrix(1 0 0 1 621.999 227.2734)" id="svgsecurity"
+        class="st6 st7">985</text>
+      <g class="st8">
+        <text transform="matrix(1 0 0 1 518.083 280.0879)"
+          class="st9 st6 st10">security code</text>
+      </g>
+      <rect x="58.1" y="378.6" class="st11" width="375.5" height="13.5" />
+      <rect x="58.1" y="405.6" class="st11" width="421.7" height="13.5" />
+      <text transform="matrix(1 0 0 1 59.5073 228.6099)" id="svgnameback"
+        class="st12 st13">Иван Иванов</text>
+    </g>
+  `;
+
+  cardBack.innerHTML = backSvgHTML;
 
   const formContainer = el('div.form-container', [
     el('div.field-container', [
